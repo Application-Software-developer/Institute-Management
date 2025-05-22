733641dca957805b828f29289c183473e306de12
SEONHU KANG <104255115+SXXNHU@users.noreply.github.com>
2025-05-22 19:03:45 +0900

    Designer.cs file upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace prototype
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSheet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSheet
+            // 
+            this.btnSheet.Location = new System.Drawing.Point(263, 160);
+            this.btnSheet.Name = "btnSheet";
+            this.btnSheet.Size = new System.Drawing.Size(240, 152);
+            this.btnSheet.TabIndex = 1;
+            this.btnSheet.Text = "장부 이동";
+            this.btnSheet.UseVisualStyleBackColor = true;
+            this.btnSheet.Click += new System.EventHandler(this.btnSheet_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSheet);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSheet;
+    }
+}
---
+++ SheetForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace prototype
+{
+    partial class SheetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSheet = new System.Windows.Forms.DataGridView();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSheet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSheet
+            // 
+            this.dgvSheet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSheet.Location = new System.Drawing.Point(24, 144);
+            this.dgvSheet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvSheet.Name = "dgvSheet";
+            this.dgvSheet.RowHeadersWidth = 51;
+            this.dgvSheet.RowTemplate.Height = 27;
+            this.dgvSheet.Size = new System.Drawing.Size(650, 206);
+            this.dgvSheet.TabIndex = 0;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(24, 61);
+            this.dtpDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(176, 21);
+            this.dtpDate.TabIndex = 1;
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(271, 61);
+            this.txtItem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(88, 21);
+            this.txtItem.TabIndex = 2;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(418, 61);
+            this.txtAmount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(88, 21);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(568, 61);
+            this.cbType.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(106, 20);
+            this.cbType.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(178, 98);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 31);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(305, 98);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(97, 31);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "수정";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(436, 98);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 31);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(592, 10);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 31);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "뒤로가기";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // SheetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 360);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.dgvSheet);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "SheetForm";
+            this.Text = "SheetForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSheet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSheet;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
