ec6db1e701a53792a606caaec9322333f23d3cfa
minseok <105479136+mkkim021@users.noreply.github.com>
2025-06-15 15:33:48 +0900

    NoticeSimple.cs

---
+++ NoticeSimple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FormNoticeBoardAndCalendar
+{
+    public partial class NoticeSimple
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+
+        public NoticeSimple(DateTime date, string title, string author, string content)
+        {
+            Date = date;
+            Title = title;
+            Author = author;
+            Content = content;
+        }
+    }
+}
