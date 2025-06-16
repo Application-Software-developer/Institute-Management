deeeaddb2aff12c499b37fe5cfe7a2cfb4ca6bdc
YeonJaeHyuk <162268704+yeontachi@users.noreply.github.com>
2025-06-17 00:28:03 +0900

    Test: (Timetable&CheckInOut&Graph)UI apply test
    시간표 기능 , 출석 기능, 결제 그래프 기능 3개 및 UI 적용 테스트

---
+++ AcademyManager/AttendanceClientForm.cs
@@ -0,0 +1,58 @@
+﻿// AttendanceClientForm.cs
+using System;
+using System.Windows.Forms;
+
+namespace AcademyManager
+{
+    public partial class AttendanceClientForm : Form
+    {
+        private TextBox inputBox;
+        private Button sendButton;
+        private AttendanceForm attendanceForm;
+
+        public AttendanceClientForm(AttendanceForm form)
+        {
+            this.attendanceForm = form;
+
+            this.Text = "출석 입력 클라이언트";
+            this.Width = 300;
+            this.Height = 150;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            InitializeLayout();
+        }
+
+        private void InitializeLayout()
+        {
+            Label label = new Label();
+            label.Text = "학생 이름 입력:";
+            label.Top = 20;
+            label.Left = 20;
+            label.Width = 100;
+            this.Controls.Add(label);
+
+            inputBox = new TextBox();
+            inputBox.Top = 20;
+            inputBox.Left = 130;
+            inputBox.Width = 120;
+            this.Controls.Add(inputBox);
+
+            sendButton = new Button();
+            sendButton.Text = "출석 전송";
+            sendButton.Top = 60;
+            sendButton.Left = 130;
+            sendButton.Width = 120;
+            sendButton.Click += SendButton_Click;
+            this.Controls.Add(sendButton);
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            string studentName = inputBox.Text.Trim();
+            if (!string.IsNullOrEmpty(studentName))
+            {
+                ((AttendanceForm)attendanceForm).MarkAttendance(studentName);
+            }
+        }
+    }
+}
---
+++ AcademyManager/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AcademyManager
+{
+    public partial class Form1 : Form
+    {
+        private Button btnTimetable;
+        private Button btnAttendance;
+        private Button btnCounseling;
+
+        public Form1()
+        {
+            this.Text = "학원 관리 프로그램 - 메인";
+            this.Width = 450;
+            this.Height = 300;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // 1. When2meet 스타일 시간표
+            btnTimetable = new Button
+            {
+                Text = "1. 시간표 작성 (When2meet)",
+                Width = 300,
+                Height = 40,
+                Top = 30,
+                Left = (this.ClientSize.Width - 300) / 2
+            };
+            btnTimetable.Click += (s, e) =>
+            {
+                // TODO: 시간표 폼으로 연결
+                TimetableForm timetableForm = new TimetableForm();
+                timetableForm.ShowDialog(this);
+
+            };
+            this.Controls.Add(btnTimetable);
+
+            // 2. 출결 확인 (통신 포함)
+            btnAttendance = new Button
+            {
+                Text = "2. 학생 출결 확인 (통신 기능 포함)",
+                Width = 300,
+                Height = 40,
+                Top = 90,
+                Left = (this.ClientSize.Width - 300) / 2
+            };
+            btnAttendance.Click += (s, e) =>
+            {
+                var serverForm = new AttendanceForm();
+                serverForm.Show();
+
+                var clientForm = new AttendanceClientForm(serverForm);
+                clientForm.Show();
+            };
+            this.Controls.Add(btnAttendance);
+
+            // 3. 상담 배정 및 드래그 이동
+            btnCounseling = new Button
+            {
+                Text = "3. 상담 시간 배정 및 드래그 이동",
+                Width = 300,
+                Height = 40,
+                Top = 150,
+                Left = (this.ClientSize.Width - 300) / 2
+            };
+            btnCounseling.Click += (s, e) =>
+            {
+                // TODO: 상담 기능 폼으로 연결
+                PaymentChartForm counselingForm = new PaymentChartForm();
+                counselingForm.ShowDialog(this);
+
+            };
+            this.Controls.Add(btnCounseling);
+        }
+    }
+}
