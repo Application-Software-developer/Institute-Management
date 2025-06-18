1996359002d152ce382df96b488583e8dceb5133
minseok <105479136+mkkim021@users.noreply.github.com>
2025-06-19 03:54:42 +0900

    Update NoticeSimple.cs

--- NoticeSimple.cs
+++ NoticeSimple.cs
@@ -1,20 +1,23 @@
-﻿using System;
+using System;
 
 namespace FormNoticeBoardAndCalendar
 {
-    public partial class NoticeSimple
+    public class NoticeSimple
     {
-        public DateTime Date { get; set; }
+        public DateTime ScheduleDate { get; set; }  // 일정 날짜
         public string Title { get; set; }
         public string Author { get; set; }
         public string Content { get; set; }
+        public string WriteDate { get; set; }       // 작성일
 
-        public NoticeSimple(DateTime date, string title, string author, string content)
+        public NoticeSimple(DateTime scheduleDate, string title, string author, string content, string writeDate)
         {
-            Date = date;
+            ScheduleDate = scheduleDate;
             Title = title;
             Author = author;
             Content = content;
+            WriteDate = writeDate;
         }
     }
+
 }
